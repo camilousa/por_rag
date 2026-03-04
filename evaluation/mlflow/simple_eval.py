18f4b70d06c1b7e8d7eed4063fbe527b892be0ea
Ubuntu <ubuntu@ip-172-31-29-207.ec2.internal>
2026-03-04 01:10:43 +0000

    feat: simple eval file example

---
+++ evaluation/mlflow/simple_eval.py
@@ -0,0 +1,35 @@
+import mlflow
+import openai
+import pandas as pd
+from typing import List
+import os
+from dotenv import load_dotenv
+from mlflow.genai.scorers import Correctness, Guidelines
+
+load_dotenv()
+
+mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))
+mlflow.set_experiment("simple eval")
+
+
+eval_data = pd.read_json("ground_truth_dataset.json")
+
+
+
+def poe_rag(question: str, context: str) -> str:
+	predictions = []
+#	print(inputs)
+#	for _, row in inputs.iterrows():
+#		predictions.append("No sé")
+	return "No sé"
+
+mlflow.genai.evaluate(
+        predict_fn=poe_rag,
+        data=eval_data,
+        scorers=[
+          Correctness()
+]
+)
+
+
+
