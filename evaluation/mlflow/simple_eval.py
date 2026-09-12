e714f1a3941b73460f6f5fe6fdeb471fdce76616
Ubuntu <ubuntu@ip-172-31-88-38.ec2.internal>
2026-09-12 14:53:11 +0000

    Update simple eval

--- evaluation/mlflow/simple_eval.py
+++ evaluation/mlflow/simple_eval.py
@@ -3,8 +3,22 @@ import openai
 import pandas as pd
 from typing import List
 import os
+import sys
+# Get the absolute path of the directory containing 'src'
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
+print(project_root)
+# Add the project root to the system path
+sys.path.insert(0, project_root)
+
+# Now you can import from src
+
+
+import mlflow
+from src.backend.generator import build_rag_chain, generate_answer
+import os
 from dotenv import load_dotenv
 from mlflow.genai.scorers import Correctness, Guidelines
+
 
 load_dotenv()
 
@@ -18,10 +32,8 @@ eval_data = pd.read_json("ground_truth_dataset.json")
 @mlflow.trace
 def poe_rag(question: str, context: str) -> str:
 	predictions = []
-#	print(inputs)
-#	for _, row in inputs.iterrows():
-#		predictions.append("No sé")
-	return "No sé"
+	answer, docs = generate_answer(question, k=5, k_candidates=8)
+	return answer
 
 mlflow.langchain.autolog()
 with mlflow.start_span(name="evaluation"):
